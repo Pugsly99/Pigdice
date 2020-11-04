fbe5e1d017cc9d0c2a23d9ab5ed4abdfb10e0d17
Pugsly99 <rylandadams@yahoo.com>
2020-11-04 13:37:25 -0800

    player object
    Co-authored-by: Glen Buck <glenbuck503@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,12 @@ function Game() {
   this.players = [];
   this.playerId = 0;
 }
-//Business logic for Game
 
+
+//Business logic for Game
+function Player(name, score) {
+  this.name = name;
+  this.score = score;
+}
 
 //UI Logic
