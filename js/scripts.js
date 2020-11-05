9f0d4a32933cfd7b48b809d99bf2294f87a842ec
Pugsly99 <rylandadams@yahoo.com>
2020-11-04 16:27:55 -0800

    functioning dice roll
    Co-authored-by: Glen Buck <glenbuck503@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -15,14 +15,30 @@ Game.prototype.assignId = function () {
 }
 
 
-  //Business logic for Game
-  function Player(name, score) {
-    this.name = name;
-    this.score = score;
+//Business logic for Game
+function Player(name, score) {
+  this.name = name;
+  this.score = score;
+}
+
+Player.prototype.diceRoll = function () {
+  let dice = Math.floor(Math.random() * 6) + 1;
+  let total = 0;
+
+  if (dice > 1) {
+    total += dice
+  } else if (dice === 1) {
+    total = 0;
+    //pass to next player
   }
+  //this.score += total;
+  //total = 0
+  this.score += total;
+}
 
 
-  //UI Logic
-  let game = new Game();
-  let newPlayer = new Player("Glen", 12);
-  game.addPlayer(newPlayer);
+//UI Logic
+let game = new Game();
+let (form#player1)= new Player((form#player1), 12);
+let player2 = new Player("Ryalnd", 2);
+game.addPlayer(newPlayer);
